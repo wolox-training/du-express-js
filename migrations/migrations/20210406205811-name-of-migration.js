23bcd921a484b0176dda9dba9ef7fa6c15796173
Daniel Urueña <daniel.uruena@wolox.co>
2021-04-07 18:12:37 -0500

    table name fixed

--- migrations/migrations/20210406205811-name-of-migration.js
+++ migrations/migrations/20210406205811-name-of-migration.js
@@ -47,6 +47,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-      return queryInterface.dropTable('users');
+      return queryInterface.dropTable('user');
   }
 };
