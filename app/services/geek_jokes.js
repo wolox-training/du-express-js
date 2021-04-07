236073fafc83fbe1d52d3922de7222441a8ddcee
Daniel Urueña <daniel.uruena@wolox.co>
2021-04-07 15:03:26 -0500

    new error added and app logger

--- app/services/geek_jokes.js
+++ app/services/geek_jokes.js
@@ -1,15 +1,21 @@
 const axios = require('axios');
 const config = require('../../config');
+const logger = require('../logger');
+const errors = require('../errors')
 
-const getRandomJoke = async () => {
+const getRandomJoke = () => {
   const options = {
     params: {
       format: 'json'
     }
   };
-  console.info('Getting random joke...');
+  logger.info('Getting random joke...');
   return axios.default.get(config.common.geek_joke_api.url, options)
-    .then(response => response.data);
+    .then(response => response.data)
+    .catch(() => {
+      logger.error('Error getting random joke');
+      throw errors.external_api_error('Error getting random joke');
+    });
 };
 
 module.exports = {
