ab73ea2341217a94d3e9a47ddec538cfdf845427
Daniel Urueña <daniel.uruena@wolox.co>
2021-04-09 11:23:45 -0500

    user model and migration fixed accord to comments

--- migrations/migrations/20210406205811-name-of-migration.js
+++ migrations/migrations/20210406205811-name-of-migration.js
@@ -9,34 +9,42 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-      return queryInterface.createTable('user', {
-        id: {
-          type: Sequelize.INTEGER,
-          unique: true,
-          autoIncrement: true,
-          primaryKey: true,
-          allowNull: false
-        },
-        firstName: {
-          type: Sequelize.STRING,
-          allowNull: false
-        },
-        lastName: {
-          type: Sequelize.STRING,
-          allowNull: false
-        },
-        email: {
-          type: Sequelize.STRING,
-          unique: true,
-          allowNull: false
-        },
-        password: {
-          type: Sequelize.STRING,
-          allowNull: false
-        }
-      }, {
-        timestamps: true
-      });
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        unique: true,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      }
+    });
   },
 
   down: (queryInterface, Sequelize) => {
@@ -47,6 +55,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-      return queryInterface.dropTable('user');
+      return queryInterface.dropTable('users');
   }
 };
