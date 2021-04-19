bda933434bddbbaf34437ab8a95909f6f43e54ae
Daniel Urueña <daniel.uruena@wolox.co>
2021-04-19 17:08:59 -0500

    documentation fixed

--- documentation/paths/user.js
+++ documentation/paths/user.js
@@ -1,38 +1,10 @@
 module.exports = {
   '/users': {
-    get: {
-      tags: ['CRUD operations'],
-      description: 'Get users',
-      operationId: 'getUsers',
-      parameters: [
-        {
-          name: 'page',
-          in: 'query',
-          schema: {
-            type: 'integer',
-            default: 1
-          },
-          required: false
-        }
-      ],
-      responses: {
-        200: {
-          description: 'Users were obtained',
-          content: {
-            'application/json': {
-              schema: {
-                $ref: '#/components/schemas/Users'
-              }
-            }
-          }
-        }
-      }
-    },
     post: {
-      tags: ['CRUD operations'],
-      description: 'Create user',
-      operationId: 'createUser',
-      parameters: [],
+      tags: ['Users'],
+      summary: 'Sign up user',
+      description: 'Sign up user',
+      operationId: 'signUp',
       requestBody: {
         content: {
           'application/json': {
@@ -40,23 +12,57 @@ module.exports = {
               $ref: '#/components/schemas/User'
             }
           }
-        },
-        required: true
+        }
       },
       responses: {
         200: {
-          description: 'New user was created'
+          description: 'User registered',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/UserResponse'
+              }
+            }
+          }
         },
         400: {
-          description: 'Invalid parameters',
+          description: 'Missing parameters',
           content: {
             'application/json': {
               schema: {
                 $ref: '#/components/schemas/Error'
               },
               example: {
-                message: 'User´s email already exists',
-                internal_code: 'invalid_parameters'
+                message: "the field 'email' is required",
+                internal_code: 'input_data_error'
+              }
+            }
+          }
+        },
+        500: {
+          description: 'Internal server error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Error'
+              },
+              example: {
+                message: 'inner exception from server',
+                internal_code: 'default_error'
+              }
+            }
+          }
+        },
+        503: {
+          description: 'Database error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Error'
+              },
+              example: {
+                message: 'Error creating user.',
+                internal_code: 'database_error'
               }
             }
           }
